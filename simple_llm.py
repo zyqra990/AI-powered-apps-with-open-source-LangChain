bee995ede65c754edee08e101b71050679027a58
Zyqra Noer Khalif <77680328+zyqra990@users.noreply.github.com>
2024-03-06 05:01:44 +0000

    komit pertama

---
+++ simple_llm.py
@@ -0,0 +1,20 @@
+import os
+import gradio as gr
+from langchain_openai import ChatOpenAI
+
+
+# Mengatur environment variable "OPENAI_API_KEY" dengan OpenAI API key milikmu. ini diperlukan untuk proses autentikasi ke OpenAI API.
+os.environ["OPENAI_API_KEY"] = "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"
+
+# Mendefinisikan jenis model 
+gpt3 = ChatOpenAI(model_name="gpt-3.5-turbo" )
+
+text = "Berikan fakta menarik tentang kentang!"
+print(gpt3.invoke(text).content)
+
+def chatbot(prompt):
+    return gpt3.invoke(prompt).content
+
+demo = gr.Interface(fn=chatbot, inputs="text", outputs="text")   
+
+demo.launch(server_name="0.0.0.0", server_port= 7860, share=True)
